e64e782ae04631a460b6517879536a6dab1f51b3
sofiaM <smejia03@gmail.com>
2020-06-23 12:26:54 -0500

    material UI, concurrently npm added

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Layout, Header, Navigation, Content } from 'react-mdl';
-import Main from './components/main';
-import { Link } from 'react-router-dom';
+import React from "react";
+// import ReactDOM form "react-dom";
+import "./App.css";
+// import { Layout, Header, Navigation, Content } from 'react-mdl';
+// import Main from './components/main';
+// import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hello World! I'm the homepage!</p>
     </div>
   );
 }
