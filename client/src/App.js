1f0a5cdb3206f65b42dd125ce9d09a1c6e2b5a7e
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-06-27 01:45:36 -0500

    Created base avatar in Illustrator, exported as SVG with unique ID names for each path. Used package SVGR to turn avatar SVG file into reusable React component. Imported avatar into AvatarTestingPage, used package Style-Component to modify avatar SVG component with in-line css and turn those into reusable components as well.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Layout, Header, Navigation, Content } from 'react-mdl';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+// import { Layout, Header, Navigation, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+import AvatarTestingPage from './components/pages/AvatarTestingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <NavTabs />
+      <Route exact path="/" component={AvatarTestingPage} />
     </div>
+  </Router>
   );
 }
 
--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import CreateLook from './createLook';
-import SearchLook from './searchLook';
-import SearchProd from './searchProd';
-import Introduction from './intro';
+import AvatarTestingPage from './pages/AvatarTestingPage';
+// import CreateLook from './createLook';
+// import SearchLook from './searchLook';
+// import SearchProd from './searchProd';
+// import Introduction from './intro';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Introduction} />
+    <Route exact path="/" component={AvatarTestingPage} />
+    {/* <Route exact path="/" component={Introduction} />
     <Route path="/createLook" component={CreateLook} />
     <Route path="/searchLook" component={SearchLook} />
-    <Route path="/searchProd" component={SearchProd} />
+    <Route path="/searchProd" component={SearchProd} /> */}
   </Switch>
 )
 
