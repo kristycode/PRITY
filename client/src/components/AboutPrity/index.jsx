2d38fac9894221a964c4399aecccc7e8635dd00e
sofiaM <smejia03@gmail.com>
2020-07-13 19:14:18 -0500

    testing for db errors

--- client/src/components/AboutPrity/index.jsx
+++ client/src/components/AboutPrity/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Container } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,7 +30,7 @@ const beautyA = {
   padding: 10,
   borderRadius: 10,
   margin: 10,
-  borderColor: "red",
+  borderColor: "#C47CA8",
   borderStyle: "outset",
 };
 const beautyB = {
@@ -59,25 +59,27 @@ export default function AboutPaper() {
   const classes = useStyles();
 
   return (
-    <Grid container justify="space-between">
-      <Grid item xs={12}>
-        <Typography variant="h4">Enhance Your Beauty...</Typography>
+    <Container>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h4">Enhance Your Beauty...</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper style={beautyA} elevation={3}>
+            Browse Color Palettes
+          </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper style={beautyA} elevation={3}>
+            Visit Your Vanity
+          </Paper>
+        </Grid>
+        <Grid xs={4}>
+          <Paper style={beautyA} elevation={3}>
+            Try Them On!
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <Paper style={beautyA} elevation={3}>
-          Browse Color Palettes
-        </Paper>
-      </Grid>
-      <Grid xs={4}>
-        <Paper style={beautyB} elevation={3}>
-          Visit Your Vanity
-        </Paper>
-      </Grid>
-      <Grid xs={4}>
-        <Paper style={beautyC} elevation={3}>
-          Try Them On!
-        </Paper>
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
