e809fb8e8251daad7717192d8250f8f8c12925d0
sofiaM <smejia03@gmail.com>
2020-07-13 20:45:43 -0500

    capitalized /Register

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,35 +1,31 @@
 import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
-import {
-  GET_ERRORS,
-  SET_CURRENT_USER,
-  USER_LOADING
-} from "./types";
+import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 
 // Register User
-export const registerUser = (userData, history) => dispatch => {
+export const registerUser = (userData, history) => (dispatch) => {
   axios
-    .post("/api/register", userData)
-    .then(res => console.log(res))
-    .then(res => history.push("/Login")) // re-direct to login on successful register
-    .catch(err =>
+    .post("/api/Register", userData)
+    .then((res) => console.log(res))
+    .then((res) => history.push("/Login")) // re-direct to login on successful register
+    .catch((err) =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response.data,
       })
     );
 };
 
 // Login - get user token
-export const loginUser = userData => dispatch => {
+export const loginUser = (userData) => (dispatch) => {
   axios
     .post("/api/login", userData)
-    .then(res => {
+    .then((res) => {
       // Save to localStorage
-// Set token to localStorage
+      // Set token to localStorage
       const { token } = res.data;
-      console.log(`1, token: ${token}`)
+      console.log(`1, token: ${token}`);
       localStorage.setItem("jwtToken", token);
       // Set token to Auth header
       setAuthToken(token);
@@ -38,31 +34,31 @@ export const loginUser = userData => dispatch => {
       // Set current user
       dispatch(setCurrentUser(decoded));
     })
-    .catch(err =>
+    .catch((err) =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response.data,
       })
     );
 };
 
 // Set logged in user
-export const setCurrentUser = decoded => {
+export const setCurrentUser = (decoded) => {
   return {
     type: SET_CURRENT_USER,
-    payload: decoded
+    payload: decoded,
   };
 };
 
 // User loading
 export const setUserLoading = () => {
   return {
-    type: USER_LOADING
+    type: USER_LOADING,
   };
 };
 
 // Log user out
-export const logoutUser = () => dispatch => {
+export const logoutUser = () => (dispatch) => {
   // Remove token from local storage
   localStorage.removeItem("jwtToken");
   // Remove auth header for future requests
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+  baseURL: "http://localhost:3001/api",
 });
 
-export const insertUser = payload => api.post(`/user`, payload);
+export const insertUser = (payload) => api.post(`/user`, payload);
 export const getAllUsers = () => api.get(`/users`);
-export const getUserById = id => api.get(`/user/${id}`);
-export const insertLook = payload => api.post(`/look`, payload);
+export const getUserById = (id) => api.get(`/user/${id}`);
+export const insertLook = (payload) => api.post(`/look`, payload);
 export const getAllLooks = () => api.get(`/looks`);
-export const getLookById = id => api.get(`/look/${id}`);
+export const getLookById = (id) => api.get(`/look/${id}`);
 export const getAllColors = () => api.get(`colors`);
-export const insertColor = payload => api.post(`/color`, payload);
-export const registerUser = payload => api.post(`/register`, payload);
-export const loginUser = payload => api.post(`/login`, payload);
+export const insertColor = (payload) => api.post(`/color`, payload);
+export const registerUser = (payload) => api.post(`/Register`, payload);
+export const loginUser = (payload) => api.post(`/login`, payload);
 
 const apis = {
-    insertUser,
-    getAllUsers,
-    getUserById,
-    insertLook,
-    getAllLooks,
-    getLookById,
-    insertColor,
-    getAllColors,
-    registerUser,
-    loginUser
-}
+  insertUser,
+  getAllUsers,
+  getUserById,
+  insertLook,
+  getAllLooks,
+  getLookById,
+  insertColor,
+  getAllColors,
+  registerUser,
+  loginUser,
+};
 
 export default apis;
